89b60786f1b13d6489237e7b818463e6823219e1
andersmartini <andersmartini2@gmail.com>
2013-05-29 18:53:48 -0300

    External JS trouble
    Externa javascriptfiler som Jquery vill inte funka i jade-views...förstår inte riktigt varför. en av 3 varianter jag prövat för att inkludera, ingen funkar!

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var stylus = require("stylus");
 var nib = require("nib");
-
+var format = require("util").format
 
 var app = express()
 function compile(str, path){
@@ -13,20 +13,28 @@ function compile(str, path){
 app.set("views",__dirname + "/views");
 app.set("view engine", "jade");
 app.use(express.logger("dev"));
+app.use(express.bodyParser())
 app.use(stylus.middleware(
 {
 	src:__dirname + "/public"
 	, 	compile: compile
 }))
-
+app.set('view options', { locals: { scripts: ['./Jquery.js', './script.js'] }}); 
+console.log("allaaahhh!")
 app.get('/', function (req, res) {
   res.render("index",{ title: "home"} )
 })
 app.get('/apa',function(req, res){
 	res.render("apa", {title: "apa"})
 })
-app.post('/apa', function(req,res){
-	console.log("PostarPosten!")
+app.get('/favicon.ico',function(req, res){
+	res.send("./favicon.ico", {title: "codeWave"})
+})
+app.get('/Jquery.js', function(req,res){
+	res.send("./Jquery.js");
+})
+app.get('/script.js', function(req,res){
+	res.send("./script.js")
 })
 
 app.use(express.static(__dirname +"/public"))
